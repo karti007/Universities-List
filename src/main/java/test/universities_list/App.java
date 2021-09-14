94482c8f95daae1bd26762f9920d9d64a451b297
Karthick Ganesan <gkarti_06@yahoo.com>
2021-09-13 23:10:49 -0400

    setting up project

---
+++ src/main/java/test/universities_list/App.java
@@ -0,0 +1,13 @@
+package test.universities_list;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
